763f2f0c293fc6fc89f9d73e0f36795d43d3dc31
Kleyton Lopes <kleyton.lop@gmail.com>
2021-09-20 16:40:52 -0300

    fix: slow up setupView because searchController bug

--- FruitStore/UI/Scenes/FruitList/FruitListView.swift
+++ FruitStore/UI/Scenes/FruitList/FruitListView.swift
@@ -15,10 +15,13 @@ class FruitListView: UIView {
 
     override init(frame: CGRect = .zero) {
         super.init(frame: frame)
-        setupView()
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupView()
     }
 }
 // MARK: - CodeViewProtocol
