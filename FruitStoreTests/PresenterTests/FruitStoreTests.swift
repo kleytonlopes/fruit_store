676d05380876b08dc56e9bb00cdedf9cdfbf843d
Kleyton Lopes <kleyton.lop@gmail.com>
2021-09-20 13:40:32 -0300

    refactor: reorganize folders

---
+++ FruitStoreTests/PresenterTests/FruitStoreTests.swift
@@ -0,0 +1,13 @@
+//
+//  FruitStoreTests.swift
+//  FruitStoreTests
+//
+//  Created by Kleyton Lopes on 16/09/21.
+//
+
+import XCTest
+@testable import FruitStore
+
+class FruitStoreTests: XCTestCase {
+
+}
